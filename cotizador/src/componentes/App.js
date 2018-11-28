4a7fb5a624245e5179b5e12ce9db5e74b2dfbb01
jleoncebra <jeffrey@cebra.cl>
2018-11-28 16:40:18 -0300

    leer los campos del form

--- cotizador/src/componentes/App.js
+++ cotizador/src/componentes/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Formulario from './Formularios';
+import {obtenerDiferenciaAnio, calcularMarca, obtenerPlan} from '../helper';
 
 class App extends Component {
   cotizarSeguro = (datos) => {
-    console.log(datos);
+    const {marca, year, plan} = datos;
+
+    //monto base de seguro
+    let resultado = 2000;
+
+    //obtener diferencia de años
+    const diferencia = obtenerDiferenciaAnio(year);
+
+    //por cada año restar el 3% al valor del seguro
+    resultado = resultado -(((diferencia * 3) * resultado) / 100);
+
+    //Americano 15%, Europeo 30% y Asiatico 5% de incremento del valor actual
+    resultado = calcularMarca(marca)*resultado;
+
+    //el plan del auto basico 20% mas y el completo 50%
+    resultado = obtenerPlan(plan)*resultado;
+
+    //para acortar los decimales a 2 y redondea
+    resultado = parseFloat(resultado).toFixed(2);
   }
 
 
